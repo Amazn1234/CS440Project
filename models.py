4ade2cfd9e269f46b4f5ec49631df88cd7310e3c
Amazn1234 <bbkaess.ck@gmail.com>
2025-04-28 12:45:05 -0700

    add models.py file to contain all DB logic

---
+++ models.py
@@ -0,0 +1,18 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+db = SQLAlchemy()
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    dueDate = db.Column(db.DateTime, nullable=False)
+    timeToDo = db.Column(db.Integer, nullable=False)
+    workDays = db.Column(db.Integer, nullable=False)
+    weekends = db.Column(db.Boolean, nullable=False)
+    workTime = db.Column(db.DateTime, nullable=False)
+    schedule = db.Column(db.String(50))
+    date_created = db.Column(db.DateTime, default=datetime.now)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
